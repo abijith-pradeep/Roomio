b9c489764ea35fd5b5b534164fb5f5bd40a3fe06
Affan Khamse <khamseaffan@gmail.com>
2024-04-30 10:46:56 -0400

    Update views.py

--- search/views.py
+++ search/views.py
@@ -14,12 +14,24 @@ def search_home(request):
         company_name = form.cleaned_data['company_name']
         building_name = form.cleaned_data['building_name']
 
+        # raw_query = """
+        # SELECT au.id, au.unit_number, au.monthly_rent, au.square_footage, au.available_date_for_move_in
+        # FROM add_post_apartmentunit AS au
+        # JOIN add_post_apartmentbuilding AS ab ON au.building_id = ab.id
+        
+        # """
+
         raw_query = """
-        SELECT au.id, au.unit_number, au.monthly_rent, au.square_footage, au.available_date_for_move_in
-        FROM add_post_apartmentunit AS au
-        JOIN add_post_apartmentbuilding AS ab ON au.building_id = ab.id
-        WHERE ab.company_name LIKE %s AND ab.building_name LIKE %s;
+                SELECT au.id, au.unit_number, au.monthly_rent, au.square_footage, au.available_date_for_move_in,
+                   ab.id AS building_id, ab.building_name, ab.company_name, ab.address_number, ab.address_street,
+                   ab.address_city, ab.address_state, ab.address_zip_code
+            FROM add_post_apartmentunit AS au
+            INNER JOIN add_post_apartmentbuilding AS ab ON au.building_id = ab.id
+            WHERE ab.company_name LIKE %s AND ab.building_name LIKE %s;
         """
+
+       
+
 
         with connection.cursor() as cursor:
             cursor.execute(raw_query, [company_name, building_name])
@@ -31,6 +43,7 @@ def search_home(request):
             favorite_ids = [item[0] for item in favorites]
 
         # Create a list of units with favorite status
+        
         units_data = [{
             'unit': {
                 'id': unit[0],
@@ -38,9 +51,31 @@ def search_home(request):
                 'monthly_rent': unit[2],
                 'square_footage': unit[3],
                 'available_date_for_move_in': unit[4],
+                'building': {
+                    'id': unit[5],
+                    'building_name': unit[6],
+                    'company_name': unit[7],
+                    'address_number': unit[8],
+                    'address_street': unit[9],
+                    'address_city': unit[10],
+                    'address_state': unit[11],
+                    'address_zip_code': unit[12]
+                }
             },
             'is_favourited': unit[0] in favorite_ids
         } for unit in units]
+
+        
+        # units_data = [{
+        #     'unit': {
+        #         'id': unit[0],
+        #         'unit_number': unit[1],
+        #         'monthly_rent': unit[2],
+        #         'square_footage': unit[3],
+        #         'available_date_for_move_in': unit[4],
+        #     },
+        #     'is_favourited': unit[0] in favorite_ids
+        # } for unit in units]
 
         context = {
             "searchForm": form,
